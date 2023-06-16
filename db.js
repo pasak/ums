e2de1fb44f746e62b7b0f738fac573ae3d45a3f4
pasak <pasaksoftware@gmail.com>
2023-06-16 16:58:36 +0700

    change db.js to Alibaba MySQL server

--- db.js
+++ db.js
@@ -1,6 +1,12 @@
 const Sequlize = require('sequelize')
+/*
 const db = new Sequlize('ums','root','036369432',{
     host:'8.222.206.50',
+    dialect:'mysql'
+})
+*/
+const db = new Sequlize('ums','root','7D93EY#Btc8j',{
+    host:'rm-gs5g33iy91f0b44eo.rwlb.singapore.rds.aliyuncs.com',
     dialect:'mysql'
 })
 
